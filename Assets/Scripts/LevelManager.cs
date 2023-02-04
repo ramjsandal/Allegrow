cec8f714f857d2d24baa21d19fb96389d602faef
ramjsandal <ramjsandal@gmail.com>
2023-02-03 19:56:04 -0500

    started level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool levelOver = false;
+    [SerializeField] private string nextScene;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadNextLevel()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    void reloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
