6e4be6d09a6a20da59b0ae7bf696ed5a0b01f4a6
ramjsandal <ramjsandal@gmail.com>
2023-02-04 12:26:27 -0500

    working on player controller and in scene1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
@@ -7,6 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed;
     [SerializeField] private float verticalSpeed;
+    [SerializeField] private float waterLevel;
+    [SerializeField] private int health;
+    [SerializeField] private int waterDecreasePerSecond;
     
     // Start is called before the first frame update
     void Start()
@@ -26,7 +30,29 @@ public class PlayerController : MonoBehaviour
         // Constantly move to the right, and move up and down
         currentTransform.position = new Vector2(currentPosition.x + Time.deltaTime * horizontalSpeed, 
             currentPosition.y + Time.deltaTime * verticalSpeed * Input.GetAxis("Vertical"));
-        
-        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If we collide with an enemy...
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+        }
+        // If we collide with a "friendly" (water, etc)
+        else if (collision.gameObject.CompareTag("Friendly"))
+        {
+            
+        }
+    }
+
+    private void decreaseWaterLevel()
+    {
+        waterLevel -= waterDecreasePerSecond * Time.deltaTime;
+    }
+
+    private void OnGUI()
+    {
+        // Draw your water level and HP
     }
 }
