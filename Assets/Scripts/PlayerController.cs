fd1390ce2ce5516be242d17a7ba3c89991a0cf23
ramjsandal <ramjsandal@gmail.com>
2023-02-04 13:22:16 -0500

    implemented lane based movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,22 +12,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int waterDecreasePerSecond;
     [SerializeField] private float waterIncrease;
     [SerializeField] private float waterDecrease;
+    [SerializeField] private GameObject lane0;
     [SerializeField] private GameObject lane1;
     [SerializeField] private GameObject lane2;
     [SerializeField] private GameObject lane3;
     [SerializeField] private GameObject lane4;
-    [SerializeField] private GameObject lane5;
-    private GameObject currentLane;
-    
+    private int currentLane;
+    private GameObject[] lanes;
 
     private GUIStyle _guiStyle = new GUIStyle();
     // Start is called before the first frame update
     void Start()
     {
+        lanes = new GameObject[5];
+        lanes[0] = lane0;
+        lanes[1] = lane1;
+        lanes[2] = lane2;
+        lanes[3] = lane3;
+        lanes[4] = lane4;
         _guiStyle.fontSize = 100;
         Transform t = this.transform;
-        t.position = new Vector2(t.position.x, lane3.transform.position.y);
-        currentLane = lane3;
+        t.position = new Vector2(t.position.x, lane2.transform.position.y);
+        currentLane = 2;
     }
 
     // Update is called once per frame
@@ -45,14 +51,17 @@ public class PlayerController : MonoBehaviour
             currentPosition.y + Time.deltaTime * verticalSpeed * Input.GetAxis("Vertical"));
          */
 
-        currentTransform.position =
-            new Vector2(currentPosition.x + Time.deltaTime * horizontalSpeed, currentPosition.y);
-        
         // If we press up, go up by one lane
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
+            currentLane -= 1;
+        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            currentLane += 1;
         }
+        
+        currentTransform.position =
+            new Vector2(currentPosition.x + Time.deltaTime * horizontalSpeed, lanes[currentLane].transform.position.y);
 
         decreaseWaterLevel();
     }
