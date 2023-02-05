49bf953e37078144d7608730f190256ec48602b0
AfrazWasTaken <afraaz.zs@gmail.com>
2023-02-04 21:28:04 -0500

    Tuned the poop to be smoother

--- Assets/BigRootSpawner.cs
+++ Assets/BigRootSpawner.cs
@@ -15,7 +15,7 @@ public class BigRootSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawnPosition = GameObject.FindGameObjectWithTag("SpawnPointBigRoot").transform;
+        spawnPosition = this.gameObject.transform;
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
         {
             startingRoot = false;
@@ -25,15 +25,28 @@ public class BigRootSpawner : MonoBehaviour
 
     void CreateNextBigRoot()
     {
-        lastSpawnedBigRoot = Instantiate(bigRootPrefab, spawnPosition.position, spawnPosition.rotation);
+        lastSpawnedBigRoot = null;
+        if (startingRoot == false)
+        {
+            lastSpawnedBigRoot = Instantiate(bigRootPrefab, spawnPosition.position + new Vector3(.75f, 0, 0), spawnPosition.rotation);
+
+        }
+        else if (startingRoot == true)
+        {
+            lastSpawnedBigRoot = Instantiate(bigRootPrefab, spawnPosition.position, spawnPosition.rotation);
+        }
+        tileSpriteRenderer = lastSpawnedBigRoot.GetComponent<SpriteRenderer>();
         SetSpriteSize();
     }
 
     void SetSpriteSize()
     {
-        if (startingRoot == false)
+        if (startingRoot == false) {
             tileSpriteRenderer.size = new Vector2(.25f, 1.125f);
-        else if (startingRoot == true)
+            
+        }
+        else if (startingRoot == true) { 
             tileSpriteRenderer.size = new Vector2(1, 1.125f);
+        }
     }
 }
