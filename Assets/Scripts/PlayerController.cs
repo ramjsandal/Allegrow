b17bca18914020515b83a83f496e7f0eb2386adb
Connor Saila <csaila@live.com>
2023-02-05 10:26:39 -0500

    jANE cadsse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -125,6 +125,8 @@ public class PlayerController : MonoBehaviour
             case "LevelEnd":
                 LevelManager.levelWon = true;
                 break;
+            default: Debug.Log("This is for you Jane");
+                break;
         }
     }
     
