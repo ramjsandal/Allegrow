dec69e544075433be5fc57d99edeee3f71dc859a
Connor Saila <csaila@live.com>
2023-02-05 11:58:48 -0500

    Title Screen Buttons

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -8,17 +8,7 @@ public class LoadSceneOnClick : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private string scene;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnMouseDown()
     {
         SceneManager.LoadScene(scene);
