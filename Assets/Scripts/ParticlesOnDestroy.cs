c9451ec247e75c0b6f2d31bf01038d59688067ea
ramjsandal <ramjsandal@gmail.com>
2023-02-05 12:30:22 -0500

    particles on water

---
+++ Assets/Scripts/ParticlesOnDestroy.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ParticlesOnDestroy : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (!this.gameObject.scene.isLoaded) return;
+        ParticleSystem effect = Instantiate(particles);
+        effect.transform.position = this.transform.position;
+        effect.Play();
+        
+        Destroy(effect.gameObject, particles.main.duration);
+    }
+}
